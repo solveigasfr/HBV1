f616301a882b926b1bcb28faf5a15d71e09256e5
Sólveig Ásta Friðriksdóttire <solveigasfr@gmail.com>
2021-11-24 15:06:53 +0000

    import,,,,

--- src/main/java/is/hi/hbv1/Controllers/HomeController.java
+++ src/main/java/is/hi/hbv1/Controllers/HomeController.java
@@ -14,16 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Controller
 public class HomeController {
