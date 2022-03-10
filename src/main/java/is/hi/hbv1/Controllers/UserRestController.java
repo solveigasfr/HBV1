bc0c9f50569711c636fba76207e1492f347a84c3
Sólveig Ásta Friðriksdóttire <solveigasfr@gmail.com>
2022-03-10 13:38:11 +0000

    restController added
    restController added in UserController

--- src/main/java/is/hi/hbv1/Controllers/UserRestController.java
+++ src/main/java/is/hi/hbv1/Controllers/UserRestController.java
@@ -4,9 +4,11 @@ import is.hi.hbv1.Persistence.Entities.User;
 import is.hi.hbv1.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class UserRestController {
 
     private UserService userService;
